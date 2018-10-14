bd25a8305d88bf6ff69517fb97df01f82d977022
Patrick Hahn <patricksilashahn@gmx.net>
2018-10-14 16:30:33 +0200

    Merge branch 'master' of /home/patrick/code/my-cfhn-presence with conflicts.

---
+++ src/main/java/de/codeforheilbronn/mycfhn/presence/configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package de.codeforheilbronn.mycfhn.presence.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://my.cfhn.it", "http://local.my.cfhn.it:4200")
+                .allowCredentials(true);
+    }
+}
